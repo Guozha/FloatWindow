5fe0a35537e0f8cc5bf3ee31ba6d05f7fa76e375
lxqxsyu <lxq_xsyu@163.com>
2014-10-09 19:00:52 +0800

    commit

--- gen/com/example/floatwindow/R.java
+++ gen/com/example/floatwindow/R.java
@@ -26,10 +26,11 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080006;
         public static final int back=0x7f080003;
-        public static final int close=0x7f080002;
         public static final int float_window_big_layout=0x7f080001;
+        public static final int float_window_grid_menu=0x7f080002;
+        public static final int menu_text=0x7f080005;
         public static final int small_window_float_layout=0x7f080004;
         public static final int start_float_window=0x7f080000;
     }
@@ -37,6 +38,7 @@ public final class R {
         public static final int activity_main=0x7f030000;
         public static final int float_window_big=0x7f030001;
         public static final int float_window_small=0x7f030002;
+        public static final int window_grid_menu_cell=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/example/floatwindow/FloatWindowBigView.java
+++ src/com/example/floatwindow/FloatWindowBigView.java
@@ -1,25 +1,35 @@
 package com.example.floatwindow;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class FloatWindowBigView extends LinearLayout{
 
 	public static int viewWidth;
 	public static int viewHeight;
+	
+	private List<String> menus;
+	private GridMenuAdapter gridMenuAdapter;
 	public FloatWindowBigView(final Context context) {
 		super(context);
 		LayoutInflater.from(context).inflate(R.layout.float_window_big, this);
 		View view = findViewById(R.id.float_window_big_layout);
 		viewWidth = view.getLayoutParams().width;
 		viewHeight = view.getLayoutParams().height;
-		Button close = (Button) findViewById(R.id.close);
-		Button back = (Button) findViewById(R.id.back);
-		close.setOnClickListener(new OnClickListener() {
+		GridView gridMenu = (GridView) view.findViewById(R.id.float_window_grid_menu);
+		Button back = (Button) view.findViewById(R.id.back);
+		/*close.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
@@ -28,7 +38,7 @@ public class FloatWindowBigView extends LinearLayout{
 				Intent intent = new Intent(getContext(), FloatWindowService.class);
 				context.stopService(intent);
 			}
-		});
+		});*/
 		back.setOnClickListener(new OnClickListener() {
 			
 			@Override
@@ -37,6 +47,55 @@ public class FloatWindowBigView extends LinearLayout{
 				MyWindowManager.createSmallWindow(getContext());
 			}
 		});
+		
+		menus = new ArrayList<String>();
+		for(int i = 0; i < 10; i++){
+			menus.add("²Ëµ¥" + i);
+		}
+		gridMenuAdapter = new GridMenuAdapter(context, menus);
+		gridMenu.setAdapter(gridMenuAdapter);
 	}
 
+	
+	class GridMenuAdapter extends BaseAdapter{
+		private List<String> menus;
+		private LayoutInflater inflater;
+		public GridMenuAdapter(Context context, List<String> menus){
+			this.menus = menus;
+			inflater = LayoutInflater.from(context);
+		}
+
+		@Override
+		public int getCount() {
+			return menus.size();
+		}
+
+		@Override
+		public Object getItem(int index) {
+			return menus.get(index);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder holder;
+			if(convertView == null){
+				convertView = inflater.inflate(R.layout.window_grid_menu_cell, null);
+				holder = new ViewHolder();
+				holder.menuText = (TextView) convertView.findViewById(R.id.menu_text);
+				convertView.setTag(holder);
+			}
+			holder = (ViewHolder) convertView.getTag();
+			holder.menuText.setText(menus.get(position));
+			return convertView;
+		}
+	}
+	
+	class ViewHolder{
+		public TextView menuText;
+	}
 }
